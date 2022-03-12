1e34104cf583eb1d4713b7f2f8f423438ab3381e
Aryan <aryan990leo@gmail.com>
2022-03-12 19:29:59 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+// import socket
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// CRUD C-CREATE-PUT . R-READ-GET U-UPDATE D-DELETE
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  });
+
+server.listen(8000, () => {
+  console.log('listening on *:8000');
+});
